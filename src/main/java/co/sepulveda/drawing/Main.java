d4699054ce127e1f3b7862ee55d7b9c0d13dab12
Carlos Sepulveda <charlesandrew621@gmail.com>
2015-07-09 08:36:27 -0500

    Initial commit
    - Add initial files to project

---
+++ src/main/java/co/sepulveda/drawing/Main.java
@@ -0,0 +1,12 @@
+package co.sepulveda.drawing;
+
+/**
+ *
+ * @author carlossepulveda
+ */
+public class Main {
+
+    public static void main(String [] args) {
+        System.out.println("Running ...");
+    }
+}
